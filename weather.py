09c7e275560fae1349b65d0034188a2a247e37cb
Jessica Torres <tjessica750@gmail.com>
2022-04-28 23:26:20 -0500

    Punto 4

--- weather.py
+++ weather.py
@@ -137,3 +137,28 @@ def matrizConfusionAuc(model, x_test, y_test, y_pred):
     fpr, tpr, _ = roc_curve(y_test, probs)
     AUC = roc_auc_score(y_test, probs)
     return matriz_confusion, AUC, fpr, tpr
+
+# 4. Crear una sección de métricas por cada modelo donde se de a conocer:
+    # a. Accuracy de Entrenamiento (Train)
+    # b. Accuracy de Validación
+    # c. Accuracy de Test (Train)
+def showMetrics(str_model,acc_train, acc_trainKN, acc_trainBC, acc_trainTC, acc_trainNB, acc_validation, acc_validationKN, acc_validationBC, acc_validationTC, acc_validationNB, acc_test, acc_testKN, acc_testBC, acc_testTC, acc_testNB):
+   
+   
+    TableMetrics = pd.DataFrame({'Metric': ['LOGISTIC REGRESSION','KNEIGHBORNS','ADABOOST CLASSIFIER','DECISION TREE','GaussianNB'],
+                     'Training Acurancy':[round(acc_train,4),round(acc_trainKN,4),round(acc_trainBC,4),round(acc_trainTC,4),round(acc_trainNB,4)],
+                     'Validation Accurancy':[round(acc_validation,4),round(acc_validationKN,4),round(acc_validationBC,4),round(acc_validationTC,4),round(acc_validationNB,4)],
+                     'Test Accurancy':[round(acc_test,4),round(acc_testKN,4),round(acc_testBC,4),round(acc_testTC,4),round(acc_testNB,4)]})
+    print("punto 4")
+    print(TableMetrics.sort_values)
+
+    
+
+
+
+
+
+
+def metrics(str_model, y_test, y_pred):
+    
+    print('')
