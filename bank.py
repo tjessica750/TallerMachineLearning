f9daa6d18dc41f496d1bf84434cb348c2677ec7d
Jessica Torres <tjessica750@gmail.com>
2022-04-28 23:20:03 -0500

    Punto 3

--- bank.py
+++ bank.py
@@ -55,3 +55,33 @@ data.month.replace(['jan','feb','mar','apr','may','jun','jul','aug','sep','oct',
 data.poutcome.replace(['unknown','failure','success','other'],[0, 1, 2, 3], inplace=True) 
 data.y.replace(['no','yes'],[0, 1], inplace=True)
 # data limpia
+
+# 3. Dividir la data en train y test
+# creacion del modelo
+x = np.array(data.drop(['y'],1))
+y = np.array(data.y)
+# 0 is not, 1 is yes
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+def metricsTraining(model, x_train, x_test, y_train, y_test):
+    kfold = KFold(n_splits=10)
+    cvscores = [] 
+    for train, test in kfold.split(x_train, y_train):
+        model.fit(x_train[train], y_train[train])
+        scores = model.score(x_train[test], y_train[test])
+        cvscores.append(scores)
+    y_pred = model.predict(x_test)
+    accuracy_train = accuracy_score(model.predict(x_train), y_train)
+    accuracy_validation = np.mean(cvscores)
+    accuracy_test = accuracy_score(y_pred, y_test)
+    return model, accuracy_validation, accuracy_test, y_pred, accuracy_train
+
+def matrizConfusionAuc(model, x_test, y_test, y_pred):
+    matriz_confusion = confusion_matrix(y_test, y_pred)
+    probs = model.predict_proba(x_test)
+    probs = probs[:, 1]
+    fpr, tpr, _ = roc_curve(y_test, probs)
+    AUC = roc_auc_score(y_test, probs)
+    return matriz_confusion, AUC, fpr, tpr
+
+
--- weather.py
+++ weather.py
@@ -105,3 +105,35 @@ data.Temp3pm = pd.cut(data.Temp3pm, [-10,0,10,20,30,40,50],
 data.RISK_MM = pd.cut(data.RISK_MM, [-50,0,50,100,150,200,250,300,350,400], 
                       labels = ['1','2','3','4','5','6','7','8','9'])
 # data limpia
+
+
+# 3. Dividir la data en train y test
+  
+# creacion del modelo
+x = np.array(data.drop('RainTomorrow', axis = 1))
+y = np.array(data.RainTomorrow)
+# 0 is not, 1 is yes
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+
+
+def metricsTraining(model, x_train, x_test, y_train, y_test):
+    kfold = KFold(n_splits=2)
+    cvscores = [] 
+    for train, test in kfold.split(x_train, y_train):
+        model.fit(x_train[train], y_train[train])
+        scores = model.score(x_train[test], y_train[test])
+        cvscores.append(scores)
+    y_pred = model.predict(x_test)
+    accuracy_train = accuracy_score(model.predict(x_train), y_train)
+    accuracy_validation = np.mean(cvscores)
+    accuracy_test = accuracy_score(y_pred, y_test)
+    return model, accuracy_validation, accuracy_test, y_pred, accuracy_train
+
+def matrizConfusionAuc(model, x_test, y_test, y_pred):
+    matriz_confusion = confusion_matrix(y_test, y_pred)
+    probs = model.predict_proba(x_test)
+    probs = probs[:, 1]
+    fpr, tpr, _ = roc_curve(y_test, probs)
+    AUC = roc_auc_score(y_test, probs)
+    return matriz_confusion, AUC, fpr, tpr
